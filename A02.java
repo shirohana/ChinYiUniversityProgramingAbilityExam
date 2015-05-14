9dc90dea4eca42d54a7f38609669c2b40f585402
Shirohana <shirohana0608@gmail.com>
2015-05-14 20:42:33 +0800

    Add A02

---
+++ A02.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class A02 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		while (scanner.hasNext()) {
+
+			int value[] = new int[2];
+			for (int i = 0; i < 2; ++i) {
+				value[i] = scanner.nextInt();
+			}
+
+			int factor = maxFactor(value[0], value[1]);
+			int multiple = value[0] * value[1] / factor;
+
+			System.out.println(factor + " " + multiple);
+
+		}
+
+	}
+
+	private static int maxFactor(int a, int b) {
+		if (a == 0) {
+			return b;
+		} else {
+			return maxFactor(b % a, a);
+		}
+	}
+}
