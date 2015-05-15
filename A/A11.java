369a90c186b5dd5bf5521dbee10cc262415bd768
Shirohana <shirohana0608@gmail.com>
2015-05-15 10:55:01 +0800

    Add A11

---
+++ A/A11.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class A11 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		while (scanner.hasNextLine()) {
+			String input = scanner.nextLine();
+			String value = input;
+
+			input = scanner.nextLine();
+			char replaceChars[] = {input.charAt(0), input.charAt(2)};
+
+			System.out.println(value.replace(replaceChars[0], replaceChars[1]));
+		}
+	}
+}
